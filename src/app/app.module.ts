5b7a99a4029e13ee426276e86dd323b6c134fb63
online.tapu@gmail.com <online.tapu@gmail.com>
2019-12-22 03:44:37 +0600

    Home-Login-registration module creatrd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './user_management/login/login.module';
+import { RegistrationModule } from './user_management/registration/registration.module';
 
 @NgModule({
   declarations: [
     AppComponent
+    // HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    LoginModule,
+    RegistrationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
