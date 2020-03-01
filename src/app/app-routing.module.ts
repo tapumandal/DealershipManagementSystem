83c120efdf83d3659133a725e1bf3eb403403f12
tapumandal <online.tapu@gmail.com>
2020-03-02 02:54:21 +0600

    fase backend is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './controller/business/dashboard/dashboard.component';
+import { HeaderComponent } from './controller/header/header.component';
+import { LoginComponent } from './user_management/login/login.component';
+import { RegistrationComponent } from './user_management/registration/registration.component';
 
 
 const routes: Routes = [
-  {path: "", component: HomeComponent, pathMatch: "full"}
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "home", component: HomeComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "registration", component: RegistrationComponent, pathMatch: "full" },
+  {
+    path: ":business-name", component: HeaderComponent, 
+    children: [
+      { path: "dashboard", component: DashboardComponent  }
+    ]
+  },
+  // {path: ":business-name/dashboard", component: DashboardComponent, pathMatch: "full"}
 ];
 
 @NgModule({
--- src/app/user_management/login/login.component.ts
+++ src/app/user_management/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+
+import { AuthenticationService } from "../../_services";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error: string;
 
-  ngOnInit() {
+  constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService
+  ) { 
+      if(this.authenticationService.currentUserValue){
+          this.router.navigate(['/']);
+      }
   }
 
+  ngOnInit() {
+
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    // alert("onSubmit");
+    console.log("onSubmit");
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.loginForm.invalid) {
+        // alert("XXXXX");
+        console.log("Invalid ");
+          return;
+      }
+
+      this.loading = true;
+      this.authenticationService.login(this.f.username.value, this.f.password.value)
+          .pipe(first())
+          .subscribe(
+              data => {
+                  console.log("DATA");
+                  this.router.navigate([this.returnUrl]);
+              },
+              error => {
+                console.log("ERROR");
+                  this.error = error;
+                  this.loading = false;
+              });
+  }
 }
